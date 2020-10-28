eb1e217c7902f746badcb310991ea35512d66799
Jude Kajura <kbjude@gmail.com>
2020-10-28 23:04:42 +0300

    Creating the calculator files

---
+++ test/calculator.js
@@ -0,0 +1,8 @@
+// const calculator = () => {
+//     if (typeof text !== 'string') {
+//         return 'The input has to be string';
+//       } if (text.length === 0) {
+//         return 'The value can not be empty';
+//       }
+    
+// }
---
+++ test/calculator.test.js
@@ -0,0 +1,11 @@
+// it('Checks for the input if a integer', () => {
+//   expect(calculator('test')).toBe('The input has to be number');
+// });
+
+// it('Checks for an empty input', () => {
+//   expect(calculator('')).toBe('The value can not be empty');
+// });
+
+// it('Checks for an addition', () => {
+//   expect(calculator.add(2, 4)).toBe(6);
+// });
