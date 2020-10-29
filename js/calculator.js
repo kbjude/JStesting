6980c4342f5abdd6ea28624c221974503db66475
Jude Kajura <kbjude@gmail.com>
2020-10-29 18:22:53 +0300

    Working tests

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,9 @@
 const calculator = ((a, b) => {
   if (typeof a || b !== 'integer') {
     return 'Input has to be an integer';
+  } if (a && b === 'integer') {
+    return a + b
   }
 });
+
 export default calculator;
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,3 +3,7 @@ import calculator from '../js/calculator';
 it('Checks for the input if a string', () => {
   expect(calculator('the', 3)).toBe('Input has to be an integer');
 });
+
+it('Checks when only one input is captured', () => {
+    expect(calculator('', 3)).toBe('Input has to be an integer');
+  });
