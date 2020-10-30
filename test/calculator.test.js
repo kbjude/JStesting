f0006b2b993a90eebca4170c9e86677ef823dec1
Jude Kajura <kbjude@gmail.com>
2020-10-30 19:38:07 +0300

    Passing tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,9 +1,26 @@
-import calculator from '../js/calculator';
+import calc from '../js/calculator';
+
 
 it('Checks for the input if a string', () => {
-  expect(calculator('the', 3)).toBe('Input has to be an integer');
+  expect(calc.add('the')).toBe('Input has to be an integer');
 });
 
 it('Checks when only one input is captured', () => {
-    expect(calculator('', 3)).toBe('Input has to be an integer');
-  });
+  expect(calc.add('')).toBe('Input has to be an integer');
+});
+
+it('Checks when right values are stored', () => {
+  expect(calc.add(3, 3)).toBe(6);
+});
+
+it('Multiplies the values when integers are passed', () => {
+  expect(calc.multiplication(2, 3)).toBe(6);
+});
+
+it('Subtracts the values when integers are passed', () => {
+  expect(calc.subtract(3, 2)).toBe(1);
+});
+
+it('Divides the values when integers are passed', () => {
+  expect(calc.divide(6, 2)).toBe(3);
+});
